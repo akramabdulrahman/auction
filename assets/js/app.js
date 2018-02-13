e7797a914aebfb2fdd3571178545bb51ddb993a7
akramabdulrahman <blackthrone91@gmail.com>
2018-02-13 16:39:17 +0300

    Initial commit

---
+++ assets/js/app.js
@@ -0,0 +1,13 @@
+Auction.init = function(){
+    $('#pinBoot').pinterest_grid({
+        no_columns: 4,
+        padding_x: 10,
+        padding_y: 10,
+        margin_bottom: 50,
+        single_column_breakpoint: 700
+    });
+
+
+}
+
+$(Auction.init);
